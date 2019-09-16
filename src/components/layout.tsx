a35e1f16feaf61e8397af0888cc32c47ec3c8a0b
Luke Brobbin <lukemichaelvocalist@gmail.com>
2019-09-16 18:24:38 +0100

    remove dotenv

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,15 +5,16 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
+import React, { FunctionComponent } from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
+import { ISiteMetaQuery } from "../types"
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
+const Layout: FunctionComponent = ({ children }) => {
+  const data = useStaticQuery<ISiteMetaQuery>(graphql`
     query SiteTitleQuery {
       site {
         siteMetadata {
