76877896be5f726b9ffa803127060db316fc2bc9
Luke Brobbin <lukemichaelvocalist@gmail.com>
2019-10-02 21:45:51 +0100

    added a11y for modal

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,8 @@
 const React = require("react")
+const Modal = require("react-modal")
 const Layout = require("./src/components/layout").default
 
 exports.wrapPageElement = ({ element, props }) => {
+  Modal.setAppElement("#___gatsby")
   return <Layout {...props}>{element}</Layout>
 }
