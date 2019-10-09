40f37d93a059f7b9c44cd8c6bc59b3ff4cfd8815
Luke Brobbin <lukemichaelvocalist@gmail.com>
2019-10-09 09:37:40 +0100

    add insta feed

--- src/components/InstaFeed.tsx
+++ src/components/InstaFeed.tsx
@@ -1,21 +1,20 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 
 export const InstaFeed = () => {
-  const script = document.createElement("script")
-  script.src =
-    "http://instafeed.assets.pixlee.com/assets/pixlee_widget_1_0_0.js"
-  script.async = true
   useEffect(() => {
     if (window !== undefined) {
+      const script = document.createElement("script")
+      script.src =
+        "http://instafeed.assets.pixlee.com/assets/pixlee_widget_1_0_0.js"
+      script.async = true
       window.PixleeAsyncInit = function() {
         Pixlee.init({ apiKey: process.env.GATSBY_PIXEL_API_KEY })
         Pixlee.addSimpleWidget({ widgetId: "22118" })
       }
-    }
-    document.body.appendChild(script)
-
-    return () => {
-      document.body.removeChild(script)
+      document.body.appendChild(script)
+      return () => {
+        document.body.removeChild(script)
+      }
     }
   }, [])
   return (
