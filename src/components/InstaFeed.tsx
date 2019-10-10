7474a4fe61f994c90056bbc77a7ca1ffeb9068ec
Luke Brobbin <lukemichaelvocalist@gmail.com>
2019-10-10 22:16:42 +0100

    Updated pixlee loading

--- src/components/InstaFeed.tsx
+++ src/components/InstaFeed.tsx
@@ -1,24 +1,8 @@
-import React, { useEffect } from "react"
+import React from "react"
+import { useInstaFeed } from "./useInstaFeed"
 
 export const InstaFeed = () => {
-  useEffect(() => {
-    console.log("here1")
-    if (typeof window !== `undefined`) {
-      console.log("here2")
-      const script = document.createElement("script")
-      script.src =
-        "https://instafeed.assets.pixlee.com/assets/pixlee_widget_1_0_0.js"
-      script.async = true
-      window.PixleeAsyncInit = function() {
-        Pixlee.init({ apiKey: process.env.GATSBY_PIXEL_API_KEY })
-        Pixlee.addSimpleWidget({ widgetId: "22118" })
-      }
-      document.body.appendChild(script)
-      return () => {
-        document.body.removeChild(script)
-      }
-    }
-  }, [])
+  useInstaFeed()
   return (
     <div>
       <div id="pixlee_container" className="instafeed__wrapper" />
