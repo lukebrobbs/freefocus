458f2ec515c1b05abfd48c258809da47e44399ca
Luke Brobbin <lukemichaelvocalist@gmail.com>
2019-10-10 07:17:37 +0100

    add insta feed

--- src/components/InstaFeed.tsx
+++ src/components/InstaFeed.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react"
 
 export const InstaFeed = () => {
   useEffect(() => {
-    if (typeof window !== undefined) {
+    if (typeof window !== `undefined`) {
       const script = document.createElement("script")
       script.src =
         "http://instafeed.assets.pixlee.com/assets/pixlee_widget_1_0_0.js"
