ad365f69de1dc32d193499398e30c7328c497a9a
Luke Brobbin <lukemichaelvocalist@gmail.com>
2021-06-15 20:03:09 +0100

    fix safari swiper bug

--- src/components/HeadlineArticles.tsx
+++ src/components/HeadlineArticles.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react"
 import SwiperCore, { A11y, Autoplay, Navigation } from "swiper"
-import { GatsbyImage, withArtDirection } from "gatsby-plugin-image"
+import { withArtDirection } from "gatsby-plugin-image"
 import { Swiper, SwiperSlide } from "swiper/react"
 import { ContentfulHeadlineArticle, ContentfulLinkText } from "../types"
+import { convertToBgImage } from "gbimage-bridge"
+import BackgroundImage from "gatsby-background-image"
 
 // Import Swiper styles
 import "swiper/swiper.min.css"
@@ -40,12 +42,13 @@ export const HeadlineArticles = ({ articles }: ContentfulHeadlineArticle) => {
       setIsOpen(true)
     }
   }
+
   return (
     <div className="-mx-4 md:mx-0 rounded-sm">
       <ArticleModal isOpen={isOpen} setIsOpen={setIsOpen} {...currentArticle} />
       <Swiper
         spaceBetween={10}
-        slidesPerView={1}
+        slidesPerView="auto"
         speed={800}
         navigation
         loop
@@ -59,6 +62,7 @@ export const HeadlineArticles = ({ articles }: ContentfulHeadlineArticle) => {
               image: article.headlineImage.gatsbyImageData,
             },
           ])
+          const bgImage = convertToBgImage(images)
 
           return (
             <SwiperSlide
@@ -81,16 +85,19 @@ export const HeadlineArticles = ({ articles }: ContentfulHeadlineArticle) => {
               aria-haspopup
               tabIndex={0}
             >
-              <GatsbyImage
-                image={images}
-                alt={article.cardTitle}
+              <BackgroundImage
+                Tag="section"
+                // Spread bgImage into BackgroundImage:
+                {...bgImage}
+                preserveStackingContext
                 className="w-full h-full"
-              />
-              <div className="absolute w-full h-full top-0 flex items-end">
-                <h2 className="uppercase md:text-md font-semibold p-2 bg-white bg-opacity-80 w-full mb-3">
-                  {article.cardTitle}
-                </h2>
-              </div>
+              >
+                <div className="absolute w-full h-full top-0 flex items-end">
+                  <h2 className="uppercase md:text-md font-semibold p-2 bg-white bg-opacity-80 w-full mb-3">
+                    {article.cardTitle}
+                  </h2>
+                </div>
+              </BackgroundImage>
             </SwiperSlide>
           )
         })}
