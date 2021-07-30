de82e5212d34169ad679493406eeb475b8968905
Luke Brobbin <lukemichaelvocalist@gmail.com>
2021-07-30 15:41:07 +0100

    update 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,37 +2,35 @@ import { Link } from "gatsby"
 import React from "react"
 import SEO from "../components/seo"
 
-const NotFoundPage = () => (
-  <>
-    <SEO title="404: Not found" />
-    <main
-      className="min-h-screen bg-cover bg-top sm:bg-top absolute top-0 left-0 w-screen"
-      style={{
-        backgroundImage:
-          'url("https://images.unsplash.com/photo-1545972154-9bb223aac798?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3050&q=80&exp=8&con=-15&sat=-75")',
-      }}
-    >
-      <div className="max-w-7xl mx-auto px-4 py-16 text-center sm:px-6 sm:py-24 lg:px-8 lg:py-48">
-        <p className="text-sm font-semibold text-black text-opacity-50 uppercase tracking-wide">
-          404 error
-        </p>
-        <h1 className="mt-2 text-4xl font-extrabold text-white tracking-tight sm:text-5xl">
-          Uh oh! I think you’re lost.
-        </h1>
-        <p className="mt-2 text-lg font-medium text-black text-opacity-50">
-          It looks like the page you’re looking for doesn't exist.
-        </p>
-        <div className="mt-6">
-          <Link
-            to="/"
-            className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-black text-opacity-75 bg-white bg-opacity-75 sm:bg-opacity-25 sm:hover:bg-opacity-50"
-          >
-            Go back home
-          </Link>
+const NotFoundPage = () => {
+  return (
+    <>
+      <SEO title="404: Not found" />
+      <div className="flex-grow flex flex-col justify-center max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="py-16">
+          <div className="text-center">
+            <p className="text-sm font-semibold text-freefocus-blue uppercase tracking-wide">
+              404 error
+            </p>
+            <h1 className="mt-2 text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">
+              Page not found.
+            </h1>
+            <p className="mt-2 text-base text-gray-500">
+              Sorry, we couldn’t find the page you’re looking for.
+            </p>
+            <div className="mt-6">
+              <Link
+                to="/"
+                className="text-base font-medium text-freefocus-blue hover:text-freefocus-secondary"
+              >
+                Go back home<span aria-hidden="true"> &rarr;</span>
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
-    </main>
-  </>
-)
+    </>
+  )
+}
 
 export default NotFoundPage
